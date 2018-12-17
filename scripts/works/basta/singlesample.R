1c731e3012243d935967198d271bf0c8d477ef65
Gianmauro Cuccuru <gmauro@crs4.it>
2018-12-17 22:42:42 +0100

    remove garbage

--- scripts/works/basta/singlesample.R
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env Rcript
-source("/home/matteo/Scrivania/crs4_pipelines/coverest/scripts/functions.R")
-bbmap_coverage_singlesample(snakemake@input[[1]],snakemake@params[[1]],snakemake@output[[1]])
